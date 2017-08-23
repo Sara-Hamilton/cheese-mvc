2daaa9b434b8e0ab4b974daedf0fdced986ab188
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2017-08-23 15:50:59 -0700

    implement edit cheese functionality

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,35 +1,24 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+import static org.launchcode.cheesemvc.models.Cheese.*;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    // static HashMap<String, String> cheeses = new HashMap<>();
-    static HashMap<String, Cheese> cheeses = new HashMap<>();
-
-    // Request path: /cheese
+      // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        // ArrayList<String> cheeses = new ArrayList<>();
-        // this was moved outside of the method and made static so that it can be accessed
-        // and used from another method in the add template
-        // cheeses.add("cheddar"); - these were used initially to add cheeses to the list
-        // cheeses.add("parmesan");
-        // cheeses.add("munster");
-
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
@@ -41,32 +30,50 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
-        if(!(cheeseName.isEmpty())) {
-            //cheeses.put(cheeseName, cheeseDescription);
-            Cheese cheese = new Cheese();
-            cheese.setName(cheeseName);
-            cheese.setDescription(cheeseDescription);
-            cheeses.put(cheeseName, cheese);
-        }
-        // redirect to /cheese - the requestMapping specified in the controller for the class
-        // to redirect somewhere else, specify the location after the :
+    //public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese){
+        CheeseData.add(newCheese);
         return "redirect:";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "Remove Cheese");
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheeseList) {
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
 
-        for (String item : cheeseList) {
-        cheeses.remove(item);
-            }
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
+        }
+
         return "redirect:";
         }
+
+    @RequestMapping(value = "edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId) {
+
+        String name = CheeseData.getById(cheeseId).getName();
+
+        model.addAttribute("cheeseToEdit", CheeseData.getById(cheeseId));
+        model.addAttribute("title", "Edit Cheese " + name + " ID " + cheeseId );
+
+        return "cheese/edit";
+    }
+
+    @RequestMapping(value = "edit", method = {RequestMethod.POST})
+    public String processEditForm(int cheeseId, String name, String description) {
+         CheeseData.getById(cheeseId).setName(name);
+         CheeseData.getById(cheeseId).setDescription(description);
+
+        //Cheese cheeseToEdit = CheeseData.getById(cheeseId);
+        //cheeseToEdit.setName(name);
+        //cheeseToEdit.setDescription(description);
+        //CheeseData.add(cheeseToEdit);
+
+        return "redirect:";
+    }
 }
